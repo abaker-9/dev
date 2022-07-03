54f2ae564eb808079913338193f3f99b692e2417
abaker9 <abaker9@ford.com>
2022-07-03 15:00:57 -0400

    update

---
+++ helper.ps1
@@ -0,0 +1,15 @@
+oc login -u abaker9 --server=https://api.sb105.caas.gcp.ford.com:6443
+
+kubectl apply -f files/d.yaml
+
+
+kubectl delete -f files/d.yaml
+
+kubectl explain devworkspace
+
+
+kubectl exec -it workspaceab8a3bd763954406-695778d7d6-n99r9 -- pwsh
+
+
+git config --global user.name "abaker9"
+git config --global user.email abaker9@gmail.com
